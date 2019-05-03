dbecc45299dd2099b64b25b8b5a7d9040f1ddd5d
leandro.qtrepador <leandro.biologo@yahoo.com.br>
2019-05-03 09:39:19 -0300

    comentario do codigo

--- app/src/main/java/com/example/leandroqtrepador/todo/Banco/ControlaBanco.java
+++ app/src/main/java/com/example/leandroqtrepador/todo/Banco/ControlaBanco.java
@@ -5,25 +5,41 @@ import android.content.Context;
 import android.database.Cursor;
 import android.database.sqlite.SQLiteDatabase;
 
+//Definição do nome da classe ControlaBanco. segue o padrão de nomes: classes começam com a primeira letra maiúscula
 public class ControlaBanco {
 
+    //Declaração do objeto "db" do tipo "SQLiteDatabase"
     private SQLiteDatabase db;
+    //Declaração de objeto "banco" do tipo "CriaBanco"
     private CriaBanco banco;
 
+    //Construtor da classe. Recebe como parâmetro o contexto.
     public  ControlaBanco(Context context){
         banco = new CriaBanco(context);
     }
 
+    //Método do tipo String (deve ser retomado um valor String).
     public String insereDado (String nome){
+        //Declara o objeto "valores" do tipo "Content/values"
         ContentValues valores;
+        //Declara a variável "resultado" do tipo "long"
         long resultado;
 
+        //Retorna o valor do método "getWritableDatabase" e adiciona no objeto "db". Diz que o banco está preparado para escrita de novos dados
         db = banco.getWritableDatabase();
+
+        //instância o objeto "valores com o construtor da classe "ContetValues"
         valores = new ContentValues();
+
+        //put é um método que recebe como parâmetro 2 Strings, a primeira é o nome da coluna do banco de dados, a segunda é o dado que será inserido na linha
         valores.put("nome", nome);
 
+        //insert é um método que recebe como parâmetro 3 valores. primeiro valor é uma String com o nome da tabela, segundo valor é uma String para os campos nulos,
+        // terceira valor é um objeto do tipo "ContentValues" com valor a ser inserido
         resultado = db.insert("tarefa", null, valores);
+        //close é um método que não recebe parâmetro nenhum. fechar a conexão do banco
         db.close();
+
 
         if (resultado == -1){
             return "Erro ao inserir dado";
@@ -33,17 +49,28 @@ public class ControlaBanco {
         }
     }
 
+    //declarando o método do tipo cursor que não recebe nenhum parâmetro
     public Cursor carregaDados(){
+        //objeto cursor do tipo Cursor
         Cursor cursor;
+
+        //um array de string com o nome campos que recebe os valores entre as chaves
         String [] campos = {"_id", "nome"};
+
+        //getReadableDatabase é um método do objeto banco que retorna um valor para o objeto db. esse método diz que o banco esta disponível para a leitura
         db = banco.getReadableDatabase();
+
+        //query é um método do db que recebe com parâmetro o que está entre parenteses. o valor retorna do método query será inserido no objeto cursor
         cursor = db.query("tarefa", campos, null, null, null, null, null);
 
+        //estrutura de decisão como parâmetro cursor for nulo faça: método do objeto cursor que não possue nenhum parâmetro, colocando o cursor na primeira posição
         if (cursor != null){
             cursor.moveToFirst();
         }
+
+        //fecha a conexão do banco
         db.close();
-        return cursor;
+        return cursor; //retorno do objeto cursor no método
     }
 
 }
--- app/src/main/java/com/example/leandroqtrepador/todo/Banco/CriaBanco.java
+++ app/src/main/java/com/example/leandroqtrepador/todo/Banco/CriaBanco.java
@@ -4,20 +4,27 @@ import android.content.Context;
 import android.database.sqlite.SQLiteDatabase;
 import android.database.sqlite.SQLiteOpenHelper;
 
+//cria uma classe CriaBanco que herda da classe SQLiteOpenHelper
 public class CriaBanco extends SQLiteOpenHelper {
 
+    //Definição das constantes para nomes do banco, tabela e colunas
     private static final String NOME_BD = "banco.bd";
     private static final String TABELA = "tarefa";
     private static final String ID = "_id";
     private static final String NOME = "nome";
     private static final int VERSAO = 1;
 
+    //Construtor da classe que recebe como parametro o contexto
+    //Deve ser criado dessa forma pois a classe CriaBanco herda da classe SQLiteOpenHelper
     public CriaBanco (Context context){
+
         super (context, NOME_BD, null, VERSAO);
     }
 
+    //Anotação JAVA para sobreescrever um método herdado de uma classe
     @Override
     public void onCreate(SQLiteDatabase db) {
+        //Cria uma String com o conteúdo de uma query para criar o banco de dados
         String sql = "CREATE TABLE " + TABELA + " ("
                 + ID + " integer primary key autoincrement, "
                 + NOME + " text"
@@ -29,8 +36,10 @@ public class CriaBanco extends SQLiteOpenHelper {
 
     @Override
     public void onUpgrade(SQLiteDatabase db, int oldVersion, int newVersion) {
+        //Query para apagar a tabela
         String sql = "DROP TABLE IF EXISTS " + TABELA;
         db.execSQL(sql);
+        //Chama o metodo onCreate novamente
         onCreate(db);
     }
 }
